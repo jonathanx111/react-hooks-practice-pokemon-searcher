2b93e0008127d8026b06d2e22c87a391a9b8b9a2
jonathanx111 <jonathanx111@gmail.com>
2021-01-17 00:45:45 -0500

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonPage from "./PokemonPage";
 
+const API_URL = "http://localhost:3001/pokemon"
+
 function App() {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(pokemonsObject => {
+        setPokemons(pokemonsObject)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <PokemonPage />
+      <PokemonPage pokemons={pokemons} setPokemons={setPokemons} apiUrl={API_URL} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Search() {
+function Search({  searchInput, setSearchInput }) {
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input onChange={e => setSearchInput(e.target.value)} value={searchInput} className="prompt" />
         <i className="search icon" />
       </div>
     </div>
